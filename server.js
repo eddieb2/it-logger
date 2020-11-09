c0aa5f59cfa2ef58b6c574f1a78769b608f73a6b
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-09 13:48:22 -0500

    prepared for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // SECTION Connect MongoDB database
 const connectDB = require('./config/db');
@@ -9,7 +10,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // SECTION Server Start
-const PORT = 5000;
+const PORT = process.env || 5000;
 app.listen(PORT, () => {
 	console.log(`
     ----------------------------
@@ -17,12 +18,28 @@ app.listen(PORT, () => {
     ----------------------------`);
 });
 
-app.get('/', (req, res) => {
-	res.send(`
-        Welcome to the It Logger API!
-    `);
-});
+// app.get('/', (req, res) => {
+// 	res.send(`
+//         Welcome to the It Logger API!
+//     `);
+// });
 
 // SECTION Routes
 app.use('/api/logs', require('./routes/logsRoute'));
 app.use('/api/techs', require('./routes/techsRoute'));
+
+// Serve static assests in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(
+				__dirname,
+				'client',
+				'build',
+				'index.html'
+			)
+		)
+	);
+}
